85b6ac12282e249cb6c91dc9524e7119d559b186
Ryan Haas <haasrr@outlook.com>
2025-11-08 20:42:20 -0500

    change webpage

---
+++ assets/js/module_default.js
@@ -0,0 +1,24 @@
+const urlParams = new URLSearchParams(window.location.search);
+const hidePlaylist = urlParams.get('noplaylist');
+const hideActivities = urlParams.get('noactivities');
+
+if (hidePlaylist) document.getElementById('playlist').style.display = 'none';
+if (hideActivities) document.getElementById('activities').style.display = 'none';
+
+let collapse = document.getElementsByClassName("collapse-container");
+for (let i = 0; i < collapse.length; i++) {
+    collapse[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        }
+        else {
+            content.style.display = "block";
+        }
+    });
+}
+
+function scrollToSection(ID) {
+    document.getElementById(ID).scrollIntoView({behavior: 'smooth'});
+}
